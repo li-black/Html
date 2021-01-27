683fe02f8d225f411e7bef55a419cfea44be7960
li-black <1436988790@qq.com>
2021-01-27 18:39:46 +0800

    Html third commit

---
+++ Html/day03_Publishdynamicpage/src/com/itheima/web/servlet/StudentServlet.java
@@ -0,0 +1,32 @@
+package com.itheima.web.servlet;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class StudentServlet implements Servlet {
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("Servlet服务");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
